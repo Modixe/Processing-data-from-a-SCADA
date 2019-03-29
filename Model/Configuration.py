6c6e0b7847d6e4b3d04c828cab465ca0bef59644
Modixe <Aleksey_97@inbox.ru>
2019-03-29 19:00:45 +0700

    1.71

--- Model/Configuration.py
+++ Model/Configuration.py
@@ -3,6 +3,7 @@ import json
 import sys
 import os
 
+
 from Model.Tag import Tag
 
 
@@ -22,18 +23,33 @@ class Configuration(object):
     _table_name = None
 
     def load(self):
+
         directory = os.path.dirname(sys.argv[0]) + "\\options.json"
 
         with open(directory, encoding='utf-8') as data_file:
             self.data = json.load(data_file, )
 
         self.__dict__ = self.data
-        self.from_date = datetime.strptime(self._from_date, '%d-%m-%Y %H:%M:%S.%f')
-        self.to_date = datetime.strptime(self._to_date, '%d-%m-%Y %H:%M:%S.%f')
+
+        try:
+            self.from_date = datetime.strptime(self._from_date, '%Y-%m-%d %H:%M:%S')
+            self.to_date = datetime.strptime(self._to_date, '%Y-%m-%d %H:%M:%S')
+        except Exception as er:
+            print("Вывод :", er)
+
         self.table_name = self._table_name
 
         for tag_name, tag_description in self.tag_names.items():
             self.tags.append(Tag(tag_name, tag_description))
+
+        # for key, val in self.tag_names.items():
+        #     self.tag_names_key = key
+        #     self.tag_names_val =
+
+
+
+
+            # print("Ключ", self.tag_names_key, "Значение", self.tag_names_val)
 
     def save_connection_setting(self, db_name, db_user, db_password, db_host, oledb_localhost, oledb_user):
         directory = os.path.dirname(sys.argv[0]) + "\\options.json"
@@ -57,20 +73,30 @@ class Configuration(object):
         if isinstance(obj, datetime):
             return obj.__str__()
 
-    def save_read_write(self, table_name, from_date, to_date, tag_names):
+    def save_read_write(self, table_name, from_date, to_date, tag_dict):
         directory = os.path.dirname(sys.argv[0]) + "\\options.json"
 
         from_date_datetime = datetime.strptime(from_date, '%d.%m.%Y %H:%M:%S')
         to_date_datetime = datetime.strptime(to_date, '%d.%m.%Y %H:%M:%S')
 
+        self.number_lines = 0
         # открываем json для чтения
         with open(directory, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
             json_data["_table_name"] = table_name
             json_data["_from_date"] = from_date_datetime
-            json_data["_to_date"] = to_date_datetime
-            json_data["tag_names"]["test"] = tag_names
             f.close()
+            json_data["_to_date"] = to_date_datetime
+            json_data["tag_names"] = tag_dict
+
+            # A = dict(zip(json_data["tag_names"], list(range(4))))
+
+
+
+
+
+
+
         # config.table_name,
         # config.from_date,
         # config.to_date,
